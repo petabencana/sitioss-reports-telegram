df06eacca6a92d114c4bdeccd05f5346eed16b82
MaanasaPriyaa <maanasa@mit.edu>
2017-05-01 20:20:28 -0400

    Code for Telegram bot Lambdas

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # cognicity-reports-telegram-lambda
 This module deploys serverless lambdas that, after user initiates a conversation via Telegram app, fetch a report card from the server and sends it to them
+
+### Install
+`npm install`
+
+### Getting started
+* Create Telegram bot as explained [here](https://core.telegram.org/bots#creating-a-new-bot) and get `BOT_TOKEN` [here](https://core.telegram.org/bots#generating-an-authorization-token). Configure its About, Description, Commands, Profile picture but hold off on setting up Webhooks until you have configured the webhook.
+* Add code for webhooks in handler.js and add it to the functions in serverless.yml file as explained [here](https://medium.com/zingle/creating-a-server-less-telegram-bot-with-aws-lambda-and-aws-api-gateway-36406471b2ca)
+* Add the lambda function to listen to relevant SNS topics and add it to the functions in serverless.yml file
+* Set up the config files as explained in the `Configuration` section.
+* Now, deploy your serverless lambdas as mentioned in the `Run` section
+* On successful deployment, you'll get a secure URL for the webhook. Now expose it via API Gateway POST method and deploy it. Set up this API's URL as the webhook as described in this [Guide](https://core.telegram.org/bots/api#setwebhook).
+* Send a text to your Telegram bot to test if it is up and running!
+* Read `Misc Notes` section to assist in configuration
+
+### Run
+`serverless deploy`
+
+### Configuration
+Save a copy of sample.env as .env in local directory with appropriate credentials
+
+* `BOT_TOKEN`: Access token created on creating a Telegram bot
+* `DEFAULT_LANG`: Current default language is English. You can add more languages here and parameterize replies for each language.
+* `CARD_PATH`: Front end's cards URL
+* `MAPSERVER`: Front end's map URL
+* `X_API_KEY`: API Key needed to make calls to the deployed server (Set it to "" during local testing)
+* `PG_CON`: Connection string for the Postgres database
+* `SERVER`: Cognicity server URL to fetch unique cardIds
+
+#### Misc Notes
+- AWS credentials are stored in bash_profile
+- Grasp "username" is userID/senderID from source networks to allow replies in conversation
+- Errors are logged to console, but not returned to user currently
+- If you want to test with your local Cognicity server instance, set up secure tunnels to localhost using ngrok and use that URL in the .env file. Install 'npm install -g ngrok'. After initializing the server, run 'ngrok http <PORT_NUMBER'. Use the https URL generated and set it in the 'SERVER' section of the env file. This allows the Lambda to interact with the server to fetch card OTL.
---
+++ handler.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const request = require('request');
+require('dotenv').config({silent:true});
+
+// GRASP card
+const options = {
+  host: process.env.SERVER,
+  path: '/cards',
+  method: 'POST',
+  port: 80,
+  headers: {
+    'x-api-key': process.env.X_API_KEY,
+    'Content-Type': 'application/json'
+  }
+};
+
+// GRASP operating regions
+const instance_regions = {
+  chn: 'chennai'
+};
+
+// Replies to user
+const replies = {
+  'en': 'Hi! Report using this link, thanks.'
+};
+
+// Confirmation message to user
+const confirmations = {
+  'en': 'Hi! Thanks for your report. I\'ve put it on the map.'
+};
+
+/*
+ * Call the Send API. The message data goes in the body. If successful, we'll
+ * get the message id in a response
+ *
+ */
+function callSendAPI(messageData, senderID) {
+  request({
+    uri: 'https://api.telegram.org/bot' + process.env.BOT_TOKEN + '/sendmessage?text=' + messageData + '&chat_id=' + senderID,
+    method: 'POST'
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var responseBody = JSON.parse(body);
+      var result = responseBody.result;
+      var recipientId = result.chat.id;
+      var messageId = result.message_id;
+
+      if (messageId) {
+        console.log('Successfully sent message with id %s to recipient %s',
+          messageId, recipientId);
+      } else {
+      console.log('Successfully called Send API for recipient %s',
+        recipientId);
+      }
+    } else {
+      console.error('Failed calling Send API', response.statusCode, body.error_code, body.description);
+    }
+  });
+}
+
+// Webhook handler - This is the method called by Telegram when user sends a message
+module.exports.webhook = (event, context, callback) => {
+  if (event.message && event.message.text && (event.message.text == "/flood" || event.message.text == "/prep")) {
+    // Form JSON request body
+    var chatID = event.message.chat.id;
+    console.log('Received flood report request via Telegram from: ' + chatID);
+    var card_request = {
+      "username": chatID.toString(),
+      "network": "telegram",
+      "language": process.env.DEFAULT_LANG
+    };
+
+    // Get a card from Cognicity server
+    request({
+      url: options.host + options.path,
+      method: options.method,
+      headers: options.headers,
+      port: options.port,
+      json: true,
+      body: card_request
+    }, function(error, response, body){
+      if (!error && response.statusCode === 200){
+        console.log('Fetched card id: ' + body.cardId);
+        //Construct the text message to be sent to the user
+        var messageText = replies[process.env.DEFAULT_LANG];
+        messageText += "\n" + process.env.CARD_PATH + event.message.text + "/" + body.cardId + "/report";
+        callSendAPI(messageText, chatID);
+        var response = {
+            statusCode: 200,
+            headers: {},
+            body: JSON.stringify({})
+        };
+        console.log('Sending success API Gateway response');
+        callback(null, response); // Send success code with empty json to avoid duplicate POST calls
+      } else {
+        var err = 'Error getting card: ' + JSON.stringify(error) + JSON.stringify(response);
+        callback(err, null); // Return error
+      }
+    });
+  }
+};
+
+module.exports.reply = (event, context, callback) => {
+  //This module listens in to SNS Telegram topic and reads the message published
+  var message = JSON.parse(event.Records[0].Sns.Message);
+  console.log('Message received from SNS topic: ' + JSON.stringify(message));
+
+  //Construct the confirmation message to be sent to the user
+  var messageText = confirmations[message.language];
+  messageText += "\n" + process.env.MAPSERVER + instance_regions[message.implementation_area] + '/' + message.report_id;
+
+  //Call Send API to confirmation message with report link to the user
+  callSendAPI(messageText, message.username);
+};
