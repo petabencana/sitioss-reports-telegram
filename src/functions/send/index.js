5440529c4fd0f21cd8c55be1781b2322be05f40f
Tomas <tomasholderness@gmail.com>
2018-04-18 12:02:14 -0400

    add callback to lambda

--- src/functions/send/index.js
+++ src/functions/send/index.js
@@ -6,9 +6,23 @@ export default (event, context, callback) => {
 
   const body = JSON.parse(event.body);
 
-  telegram.sendThanks(body).then((response) => {
+  const response = {
+    statusCode: 200,
+    headers: {},
+    body: JSON.stringify({message: 'Notification sent to user'}),
+  };
+
+  const error = {
+    statusCode: 500,
+    headers: {},
+    body: JSON.stringify({message: 'Error sending notification to user'}),
+  };
+
+  telegram.sendThanks(body).then((res) => {
     console.log('Message sent');
+    callback(null, response);
   }).catch((err) => {
     console.log('Error sending message. ' + err.message);
+    callback(null, error);
   });
 };
