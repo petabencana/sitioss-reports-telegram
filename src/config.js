31d8e6eb83468331177d3ec63e149233a3bd2e97
Tomas <tomasholderness@gmail.com>
2018-05-18 17:04:45 -0400

    add envs for monitoring

--- src/config.js
+++ src/config.js
@@ -3,6 +3,8 @@ require('dotenv').config({silent: true});
 export default {
     API_GW_WEBHOOK: process.env.API_GW_WEBHOOK,
     BOT_TOKEN: process.env.BOT_TOKEN,
+    ENDPOINT_SEND: process.env.ENDPOINT_SEND,
+    ENDPOINT_RECEIVE: process.env.ENDPOINT_RECEIVE,
     CARDS_API: process.env.CARDS_API || 'https://data.riskmap.us/cards/',
     CARDS_API_KEY: process.env.CARDS_API_KEY,
     CARDS_URL: process.env.CARDS_URL || 'https://cards.riskmap.us/flood/',
