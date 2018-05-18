39fc6b58f26ba3003ad99fd76a8e878df77f0f69
Tomas <tomasholderness@gmail.com>
2018-05-18 14:43:39 -0400

    add error catching for joi

--- src/functions/send/index.js
+++ src/functions/send/index.js
@@ -2,6 +2,7 @@ import Joi from 'joi'; // validation
 
 // Local objects
 import config from '../../config';
+import {handleResponse} from '../../lib/util';
 import Telegram from '../../lib/telegram';
 
 const _bodySchema = Joi.object().keys({
@@ -9,31 +10,35 @@ const _bodySchema = Joi.object().keys({
   instanceRegionCode: Joi.string(),
 });
 
+/**
+ * Endpoint to send telegram messages
+ * @function send
+ * @param {Object} event - AWS Lambda event object
+ * @param {Object} context - AWS Lambda context object
+ * @param {Object} callback - Callback (HTTP response)
+ */
 export default async (event, context, callback) => {
-  // Log statements
-  console.log('\n\nLoading Notify handler\n\n');
-  console.log('Incoming body: ' + event.body);
-  const telegram = new Telegram(config);
+  try {
+    // Log statements
+    console.log('\n\nLoading Notify handler\n\n');
+    console.log('Incoming body: ' + event.body);
 
-  const body = await Joi.validate(event.body, _bodySchema);
+    // Validate body
+    const body = await Joi.validate(event.body, _bodySchema);
 
-  const response = {
-    statusCode: 200,
-    headers: {},
-    body: JSON.stringify({message: 'Notification sent to user'}),
-  };
+    // Class
+    const telegram = new Telegram(config);
 
-  const error = {
-    statusCode: 500,
-    headers: {},
-    body: JSON.stringify({message: 'Error sending notification to user'}),
-  };
-
-  telegram.sendThanks(body).then((res) => {
+    // Send message
+    const result = await telegram.sendThanks(body);
+    handleResponse(callback, 200, result);
     console.log('Message sent');
-    callback(null, response);
-  }).catch((err) => {
-    console.log('Error sending message. ' + err.message);
-    callback(null, error);
-  });
+  } catch (err) {
+      if (err.isJoi) {
+        handleResponse(callback, 400, err.details[0].message);
+        console.log('Validation error: ' + err.details[0].message);
+      } else {
+        handleResponse(callback, 500, err.message);
+      }
+  }
 };
